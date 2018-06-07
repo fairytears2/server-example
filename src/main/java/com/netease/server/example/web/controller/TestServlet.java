46a3ce04fc8bcf02ed2091de2d1b49d986c62406
XIAOFANG <XIAOFANG@LAPTOP-3L7PPJ0R>
2018-06-07 10:41:41 +0800

    text

--- src/main/java/com/netease/server/example/web/controller/TestServlet.java
+++ src/main/java/com/netease/server/example/web/controller/TestServlet.java
@@ -2,10 +2,14 @@ package com.netease.server.example.web.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class TestServlet extends HttpServlet {
 
@@ -19,12 +23,12 @@ public class TestServlet extends HttpServlet {
 		System.out.println("init /hello");
 	}
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		System.out.println("service method");
-		super.service(req, resp);
-	}
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp)
+//			throws ServletException, IOException {
+//		System.out.println("service method");
+//		super.service(req, resp);
+//	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -34,5 +38,66 @@ public class TestServlet extends HttpServlet {
 		pw.print("/hello");
 		pw.close();
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		login(request, response);
+		}
 
+	public void login(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = null;
+		String userName = request.getParameter("userName");
+        String userPassword = request.getParameter("userPassword");
+        String name = null;
+        HttpSession session = request.getSession();
+        String password = (String) session.getAttribute("userPassword"); 
+        if(password!=null) {
+        	System.out.println("密码为："+password);
+//        	session.invalidate();
+        }else {
+        	session.setAttribute("userPassword", userPassword);; 
+        } 
+        userPassword = (String) session.getAttribute("userPassword");
+        Cookie usernamecookie = new Cookie("userName", userName);
+        
+        usernamecookie.setMaxAge(30*60);       
+        response.addCookie(usernamecookie);
+        
+        Cookie[] cookies = request.getCookies(); 
+        if(cookies!=null) {
+        	for (Cookie cookie:cookies) {
+        		if(cookie.getName().equals("userName")) {
+        			userName = cookie.getValue();
+        		}
+        		}
+        	}
+		if(userName!=name) {
+			System.out.println("当前用户："+userName);
+		}
+
+	try {
+        if (userName.equals("123") && userPassword.equals("123")) {
+            PrintWriter writer = response.getWriter();
+            writer.println("<html>");
+            writer.println("<head><title>用户中心</title></head>");
+            writer.println("<body>");
+            writer.println("<p>用户名：" + userName + "</p>");
+            writer.println("<p>用户说明：" + userPassword + "</p>");
+            writer.println("</body>");
+            writer.println("</html>");
+            response.setContentType("text/html;charset=UTF-8");
+            writer.close();
+        }else {
+            dispatcher = request.getRequestDispatcher("/error.html");
+            dispatcher.forward(request, response);
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+        dispatcher = request.getRequestDispatcher("/error.html");
+        dispatcher.forward(request, response);
+    }
 }
+ 
+}
+        
--- src/main/java/com/netease/server/example/web/controller/UserServlet.java
+++ src/main/java/com/netease/server/example/web/controller/UserServlet.java
@@ -2,16 +2,15 @@ package com.netease.server.example.web.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.netease.server.example.factory.ServiceFactory;
-import com.netease.server.example.meta.User;
-import com.netease.server.example.service.UserService;
+import javax.servlet.http.HttpSession;
+//import org.apache.log4j.Logger;
 
 /**
  *
@@ -20,42 +19,77 @@ import com.netease.server.example.service.UserService;
 public class UserServlet extends HttpServlet {
     private static final long serialVersionUID = 4607606190625660785L;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        process(request, response);
+//    private static Logger logger = Logger.getLogger(UserServlet.class);
+    
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		process(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-            IOException {
-        process(request, response);
-    }
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+//		logger.info("UserServlet post method is invoked.");
+		response.setContentType("text/html;charset=UTF-8");
+		process(request, response);
+	}
 
-    protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+    protected void process(HttpServletRequest request, 
+    		HttpServletResponse response) throws ServletException,
             IOException {
+    	RequestDispatcher dispatcher = null;
         String userName = request.getParameter("userName");
         String userPassword = request.getParameter("userPassword");
 
-        UserService userService = ServiceFactory.getUserService();
-        RequestDispatcher dispatcher = null;
+        HttpSession session = request.getSession();
+        String name = (String)session.getAttribute("userName");
 
-        User u = new User();
-        u.setUserName(userName);
-        u.setUserPassword(userPassword);
+        if(name!=null) {
+        	System.out.println("second login:"+name);
+        	System.out.println("username"+userName);
+        	System.out.println("userpassword"+userPassword);
+        }
+        	
+        session.setAttribute("userName", userName);
+   
+        Cookie usernamecookie = new Cookie("userName", userName);
+        Cookie pwdcookie = new Cookie("userPassword", userPassword);
+        
+        usernamecookie.setMaxAge(10*60);
+        pwdcookie.setMaxAge(10*60);
+        
+        response.addCookie(usernamecookie);
+        response.addCookie(pwdcookie);
+        
+        Cookie[] cookies = request.getCookies(); 
+        if(cookies!=null) {
+        	for (Cookie cookie:cookies) {
+        		if(cookie.getName().equals("userName")) {
+        			userName = cookie.getValue();
+        		}
+       		if(cookie.getName().equals("userPassword")) {
+        			userPassword = cookie.getValue();
+       		}
+        	}       
+    	}
+        System.out.println("userName"+userName);
+        System.out.println("userPassword"+userPassword);
 
-        try {
-            User user = userService.getUserByAccount(u);
-            if (user != null) {
+		try {
+            if (userName.equals("123") && userPassword.equals("123")) {
                 PrintWriter writer = response.getWriter();
                 writer.println("<html>");
                 writer.println("<head><title>用户中心</title></head>");
                 writer.println("<body>");
-                writer.println("<p>用户名：" + user.getUserName() + "</p>");
-                writer.println("<p>用户说明：" + user.getUserDesc() + "</p>");
+                writer.println("<p>用户名：" + userName + "</p>");
+                writer.println("<p>用户说明：" + userPassword + "</p>");
                 writer.println("</body>");
                 writer.println("</html>");
+                response.setContentType("text/html;charset=UTF-8");
                 writer.close();
-            } else {
+            }else {
                 dispatcher = request.getRequestDispatcher("/error.html");
                 dispatcher.forward(request, response);
             }
